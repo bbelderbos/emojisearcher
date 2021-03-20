10e0987090caea33e050f3fc9e62fb2c47e7bff0
Bob Belderbos <bobbelderbos@gmail.com>
2021-03-20 10:38:54 +0100

    make user signal interactive mode

--- fastemoji/script.py
+++ fastemoji/script.py
@@ -3,20 +3,36 @@ from pyperclip import copy
 
 LANGUAGE = 'en'
 QUIT = 'q'
+SIGNAL_CHAR = '.'
+PROMPT = f"""
+------------------------------------------------------------------------------------
+Type one or more emoji related words ...
+End a word with a {SIGNAL_CHAR} if you want to select an emoji if there are multiple
+matches, otherwise the first match will be picked. Type 'q' to exit.
+> """
 
 
 def get_matching_emojis(words: list[str],
-                        take_first_hit: bool = True) -> list[str]:
+                        interactive: bool = False) -> list[str]:
+    """Traverse words list finding matching emojis.
+       If there are multiple matches take the first one unless
+       interactive is set to True or the word ends with a SIGNAL_CHAR,
+       which means user specified desire for interactive lookup.
+    """
     matches = []
     for word in words:
-        emojis = get_emojis_for_word(word)
+        emojis = get_emojis_for_word(word.rstrip('.'))
         if len(emojis) == 0:
             continue
-        if len(emojis) == 1 or take_first_hit:
-            selected_emoji = emojis[0]
-        else:
+
+        interactive_mode = word.endswith(SIGNAL_CHAR) or interactive
+        if len(emojis) > 1 and interactive_mode:
             selected_emoji = user_select_emoji(emojis)
+        else:
+            selected_emoji = emojis[0]
+
         matches.append(selected_emoji)
+
     return matches
 
 
@@ -30,7 +46,7 @@ def user_select_emoji(emojis: list[str]) -> str:
         try:
             for i, emo in enumerate(emojis, start=1):
                 print(i, emo)
-            user_input = input("Select which emoji you want? ")
+            user_input = input("Select the number of the emoji you want: ")
             idx = int(user_input)
             return emojis[idx - 1]
         except ValueError:
@@ -40,7 +56,7 @@ def user_select_emoji(emojis: list[str]) -> str:
 
 def main():
     while True:
-        user_input = input("Type one or more emoji related words (type 'q' for exit): ")
+        user_input = input(PROMPT)
         user_input = user_input.lower()
         if user_input == QUIT:
             print('Bye')
